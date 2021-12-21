0c460c05a6a0bdcb0cc2318f0e4587b7e1a1e851
maestapereira <sm5150@columbia.edu>
2021-12-21 17:34:10 -0500

    Cleaned up confusion matrix plotting code.

--- project_code/plot_confusion_matrix.py
+++ project_code/plot_confusion_matrix.py
@@ -6,7 +6,7 @@
 # In[ ]:
 
 
-#### import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -16,105 +16,71 @@ fp = 131559
 fn = 272144
 tp = 424130
 
-actual_neg = 12149013
-actual_pos = 696274
+actual_neg = tn + fp # 12149013
+actual_pos = tp + fn # 696274
 
-predic_neg = 12289598
-predic_pos = 555689
-
-
-# In[22]:
-
-
-conf_matrix = [[tn, fp], [fn, tp]]
-conf_matrix
-
-
-# In[21]:
-
-
-conf_matrix_norm = [[tn/actual_neg, fp/actual_pos], [fn/actual_neg, tp/actual_pos]]
-conf_matrix_norm
-
-
-# In[26]:
-
-
-conf_matrix_norm2 = [[tn/predic_neg, fp/predic_pos], [fn/predic_neg, tp/predic_pos]]
-conf_matrix_norm2
-
-
-# In[11]:
-
-
-np.linspace(0, 2, 2)
+predic_neg = tn + fn # 12289598
+predic_pos = tp + fp # 555689
 
 
 # In[28]:
 
 
+# confusion matrix
+cm = [[tn, fp], [fn, tp]]
+
 fig, ax = plt.subplots(figsize=(15, 15))
-# plot confusion matrix
-shw = ax.imshow(conf_matrix, cmap='Blues')
+# plot
+shw = ax.imshow(cm, cmap='Blues')
 ax.set_title('Confusion Matrix')
-
 bar = plt.colorbar(shw)
-  
 # show plot with labels
 plt.xlabel('Predicted label')
 plt.ylabel('True label')
 bar.set_label('accuracies')
 plt.xticks(ticks=[0, 1], labels=['false', 'true'], rotation='vertical')
 plt.yticks(ticks=[0, 1], labels=['false', 'true'])
-
-# shw.set_clim(0, 1)
-plt.show()
-
-
-# In[33]:
-
-
-fig, ax = plt.subplots(figsize=(15, 15))
-# plot confusion matrix
-shw = ax.imshow(conf_matrix_norm2, cmap='Blues')
-ax.set_title('Confusion Matrix: normalized by predicted data')
-
-bar = plt.colorbar(shw)
-  
-# show plot with labels
-plt.xlabel('Predicted label')
-plt.ylabel('True label')
-bar.set_label('accuracies')
-plt.xticks(ticks=[0, 1], labels=['false', 'true'], rotation='vertical')
-plt.yticks(ticks=[0, 1], labels=['false', 'true'])
-
-shw.set_clim(0, 1)
 plt.show()
 
 
 # In[31]:
 
 
+# confusion matrix normalized by actual data totals
+cm_norm_by_actual = [[tn/actual_neg, fp/actual_pos], [fn/actual_neg, tp/actual_pos]]
 fig, ax = plt.subplots(figsize=(15, 15))
-# plot confusion matrix
+# plot
 shw = ax.imshow(conf_matrix_norm, cmap='Blues')
 ax.set_title('Confusion Matrix: normalized by actual data')
-
 bar = plt.colorbar(shw)
-  
 # show plot with labels
 plt.xlabel('Predicted label')
 plt.ylabel('True label')
 bar.set_label('accuracies')
 plt.xticks(ticks=[0, 1], labels=['false', 'true'], rotation='vertical')
 plt.yticks(ticks=[0, 1], labels=['false', 'true'])
-
+# set limits to 0 and 1
 shw.set_clim(0, 1)
 plt.show()
 
 
-# In[ ]:
+# In[33]:
 
 
-
+# confusion matrix normalized by predicted data totals
+cm_norm_by_pred = [[tn/predic_neg, fp/predic_pos], [fn/predic_neg, tp/predic_pos]]
+fig, ax = plt.subplots(figsize=(15, 15))
+# plot confusion matrix
+shw = ax.imshow(cm_by_pred, cmap='Blues')
+ax.set_title('Confusion Matrix: normalized by predicted data')
+bar = plt.colorbar(shw)
+# show plot with labels
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+bar.set_label('accuracies')
+plt.xticks(ticks=[0, 1], labels=['false', 'true'], rotation='vertical')
+plt.yticks(ticks=[0, 1], labels=['false', 'true'])
+# set limits to 0 and 1
+shw.set_clim(0, 1)
+plt.show()
 
